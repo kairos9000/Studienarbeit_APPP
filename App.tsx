bd95acf7e13aab9cca5bc6788e1cb463d105a5c5
Philip Bartmann <p.bartmann@oth-aw.de>
2022-11-23 13:59:07 +0100

    add map and show location

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
 import * as React from "react";
 import MapView from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
+import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
+    // const [location, setLocation] = useState<Location.LocationObject | null>(null);
+
+    const getAPI = () => {
+        fetch("https://parken.amberg.de/wp-content/uploads/pls/pls.xml")
+            .then((response) => response.text())
+            .then((text) => {
+                const parser = new XMLParser();
+                let xml = parser.parse(text);
+                console.log(xml.Daten.Parkhaus);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                console.error("Permission to access location was denied");
+                return;
+            }
+
+            // let location = await Location.getCurrentPositionAsync({});
+            // setLocation(location);
+        })();
+    }, []);
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            <MapView style={styles.map} showsUserLocation followsUserLocation />
+            <Button title="getAPI" onPress={getAPI}></Button>
         </View>
     );
 }
@@ -19,6 +50,9 @@ const styles = StyleSheet.create({
     },
     map: {
         width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height,
+        height: Dimensions.get("window").height - 100,
+    },
+    button: {
+        flex: 1,
     },
 });
