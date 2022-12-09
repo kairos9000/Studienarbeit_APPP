333a2d7b443617a6a15fa5468ede8f6c646ce774
Philip Bartmann <p.bartmann@oth-aw.de>
2022-12-09 14:28:43 +0100

    tested with emulator and zoom to start location

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import MapView from "react-native-maps";
+import MapView, { Region } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
 import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser";
 import { useEffect, useState } from "react";
@@ -7,6 +7,7 @@ import * as Location from "expo-location";
 
 export default function App() {
     // const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [region, setRegion] = useState<Region | undefined>(undefined);
 
     const getAPI = () => {
         fetch("https://parken.amberg.de/wp-content/uploads/pls/pls.xml")
@@ -29,13 +30,18 @@ export default function App() {
                 return;
             }
 
-            // let location = await Location.getCurrentPositionAsync({});
-            // setLocation(location);
+            let location: Location.LocationObject = await Location.getCurrentPositionAsync({});
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.05,
+            });
         })();
     }, []);
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} showsUserLocation followsUserLocation />
+            <MapView style={styles.map} showsUserLocation followsUserLocation region={region} />
             <Button title="getAPI" onPress={getAPI}></Button>
         </View>
     );
---
+++ festeDatenParkhaus.ts
@@ -0,0 +1,1 @@
+// hier feste Dateien als ts-Objekt exportieren, um einfacher in Datenbank einlesen zu können
